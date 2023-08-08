3ed1e44396c4e410d8e4753cbe2214ee337205c0
Rostyslav Bohomaz <rostyslav.db@gmail.com>
2023-08-08 13:56:31 +0300

    home and aboud page and styles

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
-from typing import Literal, Optional
+from typing import Literal, Optional, Annotated
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.params import Query
 from fastapi.responses import RedirectResponse
 from fastapi.requests import Request
@@ -17,12 +17,28 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 template_response = Jinja2Templates(directory="templates", trim_blocks=True, lstrip_blocks=True).TemplateResponse
 
 
+def get_years_old():
+    from datetime import datetime, timezone, timedelta
+
+    tz = timezone(offset=timedelta(hours=+3), name="Kyiv")
+    
+    return (datetime.now(tz) - datetime(1996, 7, 30, tzinfo=tz)).days // 365
+
+
 @app.get("/")
-def index(request: Request, theme: Optional[Literal["light", "dark"]] = Query(None)):
+def index(
+    request: Request,
+    years_old: Annotated[int, Depends(get_years_old)],
+    theme: Optional[Literal["light", "dark"]] = Query(None),
+):
     if theme is None:
-        return template_response("index.jinja", {"request": request})
+        return template_response("index.jinja", {"request": request, "years_old": years_old})
     else:
         response = RedirectResponse(request.headers.get("referer", "/"))
         response.set_cookie("theme", theme)
         return response
 
+
+@app.get("/about")
+def about(request: Request):
+    return template_response("about.jinja", {"request": request})
