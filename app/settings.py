42fe9a6ecee8a3a3751c9744030880d12235250d
Rostyslav Bohomaz <rostyslav.db@gmail.com>
2023-08-09 14:50:46 +0300

    update settings

--- app/settings.py
+++ app/settings.py
@@ -1,19 +1,30 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from os import getenv
+
+from pydantic import Field
+from pydantic_settings import BaseSettings
 
 
 __all__ = ["settings"]
 
 
-class Settings(BaseSettings):
-    base_url: str
+DEFAULT_HOST = "localhost"
+DEFAULT_PORT = 5000
 
-    host: str = "localhost"
-    port: int = 5000
+
+def get_default_base_url() -> str:
+    host = getenv('HOST', DEFAULT_HOST)
+    port = getenv('PORT', DEFAULT_PORT)
+    return f"http://{host}:{port}"
+
+
+class Settings(BaseSettings):
+    host: str = DEFAULT_HOST
+    port: int = DEFAULT_PORT
+
+    base_url: str = Field(default_factory=get_default_base_url)
+
     reload: bool = False
     debug: bool = False
     workers: int = 1
-
-    model_config = SettingsConfigDict(env_file=".env")
-
 
 settings = Settings()
